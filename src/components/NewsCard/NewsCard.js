8320f0a5a2210a0dcb98a5ae215983d635e01930
salonisingh-23 <salonisingh5723@gmail.com>
2022-05-03 13:06:21 +0530

    first commit

---
+++ src/components/NewsCard/NewsCard.js
@@ -0,0 +1,42 @@
+import React,{useState,useEffect,createRef} from 'react';
+import {Card, CardActions,CardActionArea, CardContent,CardMedia, Button,Typography} from '@material-ui/core';
+import useStyles from './style.js';
+
+
+const NewsCard = ({article:{description,publishedAt,source,title,url,urlToImage},i, activeArticle}) => {
+    const classes=useStyles();
+    const [elRefs,setElRefs]=useState([]);
+const scrollToRef=(ref)=>window.scroll(0,ref.current.offsetTop-50);
+
+    useEffect(()=>{
+        setElRefs((refs)=>Array(20).fill().map((_,j)=>refs[j]||createRef()));
+    },[]);
+    useEffect(()=>{
+        if(i===activeArticle&&elRefs[activeArticle])
+        { 
+            scrollToRef(elRefs[activeArticle]);
+        }
+    },[i,activeArticle,elRefs]);
+    
+  return (
+   <Card ref={elRefs[i]} className={activeArticle===i?classes.activeCard:null}>
+       <CardActionArea href={url} target="_blank">
+       <CardMedia className={classes.media} image={urlToImage||'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSeDLUgPm6eqlc3xZzykaaMRKvUUlMVaaiUlA&usqp=CAU' }/>
+       <div> 
+       <Typography variant="body2" color="textSecondary" component="h2">{(new Date(publishedAt)).toDateString()}</Typography>
+       <Typography variant="body2" color="textSecondary" component="h2">{source.name}</Typography>
+       </div>
+       <Typography className={classes.title} gutterBottom variant="h5">{title}</Typography>
+       <CardContent>
+           <Typography variant="body2" color="textSecondary" component="p">{description}</Typography>
+       </CardContent>
+       </CardActionArea>
+       <CardActions className={classes.cardActions}>
+           <Button size="small" color="primary"></Button>
+           <Typography variant="h5" color="textSecondary"></Typography>
+       </CardActions>
+   </Card>
+  );
+};
+
+export default NewsCard;
